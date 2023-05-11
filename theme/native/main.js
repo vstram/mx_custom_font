fd6b48300dc79157ca899af0186985df1a241ed1
vstram <vstram@gmail.com>
2023-05-11 08:41:20 -0300

    Initial Commit

---
+++ theme/native/main.js
@@ -0,0 +1,51 @@
+export const ubuntuFontFamily = {
+    bold: "Ubuntu-Bold",
+    boldItalic: "Ubuntu-BoldItalic",
+    italic: "Ubuntu-Italic",
+    regular: "Ubuntu-Regular",
+};
+
+export const ubuntuMediumFontFamily = {
+    regular: "Ubuntu-Medium",
+    italic: "Ubuntu-MediumItalic",
+};
+
+export const ubuntuLightFontFamily = {
+    regular: "Ubuntu-Light",
+    italic: "Ubuntu-LightItalic",
+};
+
+export const sacramentoFontFamily = {
+    regular: "Sacramento-Regular",
+};
+
+export const textUbuntuNormal = {
+    text: {
+        fontFamily: ubuntuFontFamily.regular,
+        fontWeight: 'normal',
+    },
+};
+
+export const textUbuntuBold = {
+    text: {
+        fontFamily: ubuntuFontFamily.bold,
+        fontWeight: 'bold',
+    }
+};
+
+export const textUbuntuItalic = {
+    text: {
+        fontFamily: ubuntuFontFamily.italic,
+        fontWeight: 'normal',
+    },
+};
+
+export const textSacramentoNormal = {
+    text: {
+        fontFamily: sacramentoFontFamily.regular,
+        fontWeight: 'normal',
+        fontSize: 30,
+        lineHeight: 40,
+    },
+};
+
